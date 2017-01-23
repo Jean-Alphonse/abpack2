8ecb8707f179ee6dbd97f91a40b2902a4401336b
Meowlala <pt.fackler@gmail.com>
2017-01-23 09:46:01 -0600

    Update skeleton some more
    More sorting of the code

--- main.lua
+++ main.lua
@@ -1,3 +1,61 @@
 local Alphabirth = {}
 Isaac.RegisterMod(Alphabirth,"Alphabirth Pack 2",1)
 Isaac.DebugString("I'm alive!")
+
+----------------------------------------
+-- Config
+----------------------------------------
+
+----------------------------------------
+-- Initialize the RNG
+----------------------------------------
+math.randomseed(Random())
+math.random();math.random();math.random();
+
+----------------------------------------
+-- Costume Declaration
+----------------------------------------
+
+----------------------------------------
+-- Entity Flag Declaration
+----------------------------------------
+-- use EntityFlag.FLAG_YOUR_FLAG = 1 << FlagID
+
+----------------------------------------
+-- Curse Declaration
+----------------------------------------
+-- use LevelCurse.CURSE_YOUR_CURSE = 1 << CurseID
+
+----------------------------------------
+-- Active Declaration
+----------------------------------------
+-- use CollectibleType.COLLECTIBLE_YOUR_ITEM = ItemID
+
+----------------------------------------
+-- Passive Declaration
+----------------------------------------
+-- use CollectibleType.COLLECTIBLE_YOUR_ITEM = ItemID
+
+----------------------------------------
+-- Trinket Declaration
+----------------------------------------
+-- use TrinketType.TRINKET_YOUR_ITEM = TrinketID
+
+----------------------------------------
+-- Variables that need to be loaded early
+----------------------------------------
+
+
+
+
+----------------------------------------
+-- Post-Update Callback
+----------------------------------------
+function Alphabirth:modUpdate()
+end
+
+----------------------------------------
+-- Callbacks
+----------------------------------------
+Alphabirth:AddCallback(ModCallbacks.MC_POST_UPDATE, Alphabirth.modUpdate)
+
